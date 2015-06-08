0b35ef02506fd01cd8bfb4833e119afc29cd7342
Amit Nihalani <ami.nihalani@gmail.com>
2015-06-08 16:53:44 -0400

    Added logic for Devices and RankSource
    - Added logic for Devices and RankSource
    - Made changes after code review.
    - Added DAO classes

--- src/main/java/sql/JDBCConnection.java
+++ src/main/java/sql/JDBCConnection.java
@@ -1,11 +1,9 @@
 package sql;
 
-import utils.StringUtility;
 import utils.Util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 /**
@@ -13,6 +11,7 @@ import java.util.Properties;
  */
 public class JDBCConnection {
     public static final Properties properties = Util.readProperties(Util.PROPS_FILE);
+    public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     /**
      * Method to create a JDBC connection and return it.
      * 
@@ -21,19 +20,18 @@ public class JDBCConnection {
     public static Connection getConnection() {
 
         try {
-            // STEP 2: Register JDBC driver
-            Class.forName(StringUtility.JDBC_DRIVER);
-            // STEP 3: Open a connection and return it
+            // Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+            //  Open a connection and return it
             return DriverManager.getConnection(properties.getProperty("DB_URL"), properties.getProperty("DB_USER"),
                     properties.getProperty("DB_PASSWORD"));
-        } catch (SQLException se) {
-            // Handle errors for JDBC
-            se.printStackTrace();
         } catch (Exception e) {
+            // Handle errors for JDBC
             // Handle errors for Class.forName
+            System.out.println("Exception in JDBCConnection.getCOnnection");
             e.printStackTrace();
         }
         return null;
     }
-}// end JDBCExample
+}
 
